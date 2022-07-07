1f54e8ef824bf53d4bb4a41fdc4b978de3849aa1
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-07-07 15:58:57 +0300

    Добавляет предпосмотр для фотографий

--- js/form.js
+++ js/form.js
@@ -3,6 +3,47 @@ const advertFormFieldsets = advertForm.querySelectorAll('fieldset');
 const advertFormSlider = advertForm.querySelector('.ad-form__slider');
 const advertFormSubmit = advertForm.querySelector('.ad-form__submit');
 
+const avatarImageChooser = advertForm.querySelector('#avatar');
+const avatarImage = advertForm.querySelector('.ad-form-header__preview img');
+const advertImageChooser = advertForm.querySelector('#images');
+const advertImage = advertForm.querySelector('.ad-form__photo');
+
+const FILE_TYPES = ['jpeg', 'jpg', 'png'];
+
+// Устанавливает превью картинки
+const setPreview = (chooser, image) => {
+  const file = chooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((fileType) => fileName.endsWith(fileType));
+
+  if (matches) {
+    image.src = URL.createObjectURL(file);
+  }
+};
+
+// Предпросмотр аватара
+avatarImageChooser.addEventListener('change', () => {
+  setPreview(avatarImageChooser, avatarImage);
+});
+
+// Создает фото объявления
+const createAdvertPhoto = (source = '#', width = 70, height = 70, alt = 'Фотография жилья') => {
+  const photo = document.createElement('img');
+  photo.classList.add('popup__photo');
+  photo.src = source;
+  photo.width = width;
+  photo.height = height;
+  photo.alt = alt;
+  advertImage.append(photo);
+  return photo;
+};
+
+// Предпросмотр фото объявления
+advertImageChooser.addEventListener('change', () => {
+  setPreview(advertImageChooser, createAdvertPhoto());
+});
+
 // Блокирует форму создания нового объявления и поля внутри
 const disableAdvertForm = () => {
   advertForm.classList.add('ad-form--disabled');
